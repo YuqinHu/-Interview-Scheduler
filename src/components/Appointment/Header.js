4956ffe19b91ec213c12be64fece054f35f8746c
YuqinHu <h878688736@gmail.com>
2023-03-23 22:04:13 -0700

    finish appointment schuler

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -2,12 +2,9 @@ import React from "react";
 
 export default function Header(props) {
   return(
-  <main className="appointment__add">
-    <img
-      className="appointment__add-button"
-      src="images/add.png"
-      alt="Add"
-    />
-  </main>
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">{props.time}</h4>
+      <hr className="appointment__separator" />
+    </header>
   )
 };
