60d22b262089f25eb8465a012d7ce6a850d5ddf9
YuqinHu <h878688736@gmail.com>
2023-03-24 19:56:33 -0700

    Transforming Data with Selectors and Requesting the Days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
 import DayList from "./DayList";
+
+
 
 const appointments = {
   "1": {
@@ -44,27 +47,16 @@ const appointments = {
 
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(response => {
+        setDays(response.data);
+      })
+  }, []);
   const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
